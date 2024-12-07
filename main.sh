14a414c4cd2a4d081ab28b3ec6077a37fec08093
Deevanshik <137674395+Deevanshik@users.noreply.github.com>
2024-12-07 21:54:11 +0530

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,8 +1,7 @@
-python 
-coco_dataset_resize.py 
---images_dir="/kaggle/input/coco-2017-dataset/coco2017/train2017/" /
---annotations_file="/kaggle/input/annotation-file/instances_train2017_seen.json" /
---image_width=512 /
---image_height=512 /
---output_ann_file="resized_annotations.json" /
+python coco_dataset_resize.py \
+--images_dir="/kaggle/input/coco-2017-dataset/coco2017/train2017/" \
+--annotations_file="/kaggle/input/annotation-file/instances_train2017_seen.json" \
+--image_width=512 \
+--image_height=512 \
+--output_ann_file="resized_annotations.json" \
 --output_img_dir="/kaggle/working/COCO-dataset-resize/output/"
